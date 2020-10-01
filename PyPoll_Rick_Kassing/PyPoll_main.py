bfe4a429474e56b9a6e2a8bef4e26969f45c27c5
Rick Kassing <rkassing@gmail.com>
2020-09-30 22:10:03 -0400

    added completed homework

--- PyPoll_Rick_Kassing/PyPoll_main.py
+++ PyPoll_Rick_Kassing/PyPoll_main.py
@@ -10,46 +10,55 @@ with open(election_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
 
     header = next(csvreader)
-
-    
     total_votes=0
 
     # Create a dictionary to hold the candidates names.
     candidates = {}
     max_votes = 0
-    winner = {}
+    winner = ""
     list2 = []
-# Create a dictionary using the built-in function.
+    #candidates = []
+    candidate = {}    
+    # Create a dictionary using the built-in function.
     candidates = dict()
 
     # Total Number of Votes Cast
     for row in csvreader:
         total_votes=total_votes+1
+    #Find the number of unique candiates
         if row[2] not in list2:
+           #List of unique candidates in row2 
             list2.append(row[2])
             candidates[row[2]] = 0
+            #If candidate is not unite, add vote to thier count        
         candidates[row[2]] = candidates[row[2]]+1
-
+    # Calaculate the number of votes/candidate
         percent_dict = {}
-    for x in candidate_dict:
-        percent_dict[x] = round((candidate_dict[x] / total_votes) * 100, 2)
-        votes = candidate_dict.get(x)
+    # Calaculate the % of votes/candidate
+    for x in candidates:
+        percent_dict[x] = ((candidates[x] / total_votes) * 100)
+        votes = candidates.get(x)
         if votes > max_votes:
              max_votes = votes
              winner = x
-output = f"{x} : {percent_dict[x]}% , {votes} \n "
+
 print(f"Election Results")
 print(f"-------------------------")
 print(f"Total Votes: {(total_votes)}")
 print(f"-------------------------")
-print(f"({candidates)})"
-# print(f"Winner : {(winner)}")
+for x in candidates:
+    print(str(x), round(percent_dict[x],5), (candidates[x]))
+print(f"-------------------------")
+print(f"Winner : {(winner)}")
+print(f"-------------------------")
 
-#  Open the output file
-with open(Final_Results.csv, "w") as datafile:
-# Write the lines
-    writer = csv.writer(datafile)
-    writer.writerow("Election Results" + "\n")
-    writer.writerow("-------------------------" + "\n")
-    writer.writerow("Total Votes" + str(total_votes) + "\n")
-    writer.writerow("-------------------------" + "\n")
+Final_Results= open('Final_Results.txt', 'w')
+
+# Write the lines for the text file
+Final_Results.write(f"Election Results" + "\n")
+Final_Results.write("-------------------------" + "\n")
+Final_Results.write("Total Votes: {(total_votes)}" + "\n")
+Final_Results.write("-------------------------" + "\n")
+Final_Results.write("-------------------------" + "\n")
+Final_Results.write("Winner : {(winner)}" + "\n")
+Final_Results.write("-------------------------" + "\n")
